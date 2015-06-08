25c94a68247ff865f467ac03b6dedef5d1419ed4
ramadimasatria <rama.dimasatria@tapestrix.net>
2015-06-08 14:56:44 +0700

    navigation & map icon

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,41 @@
 $(document).ready(function () {
-  $('#content').fullpage();
+  // Function to run after section changed
+  // All animation goes here
+  var afterLoadHandler = {
+    'section1': function () {
+      $('#navigation, #mapIcon').fadeOut();
+    },
+
+    'section2': function () {
+      $('#navigation, #mapIcon').fadeIn();
+    },
+
+    'section3': function () {
+    },
+
+    'section4': function () {
+    },
+
+    'section5': function () {
+    },
+
+    'section6': function () {
+    },
+
+    'section7': function () {
+    },
+
+    'section8': function () {
+    },
+  };
+
+  // Initiate fullpage js
+  $('#content').fullpage({
+    anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7', 'section8'],
+
+    afterLoad: function (anchorLink, index) {
+      afterLoadHandler[anchorLink]();
+    }
+  });
+
 });
