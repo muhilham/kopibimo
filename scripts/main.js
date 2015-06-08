6d5a3f168bab14f3c6d66c2fc6fd4809ad9c2cb8
ramadimasatria <ramadimasatria@gmail.com>
2015-06-08 22:20:25 +0700

    added bg images and logo

--- scripts/main.js
+++ scripts/main.js
@@ -3,42 +3,56 @@ $(document).ready(function () {
   // All animation goes here
   var afterLoadHandler = {
     'home': function () {
+      $('.anim-s1-bg').fadeIn();
     },
 
     'journey': function () {
-      $('.anim-s2-1').fadeIn();
-      $('.anim-s2-2').delay(600).fadeIn(100).addClass('fadeInLeft');
+      $('.anim-s2-bg').fadeIn();
+
+      $('.anim-s2-1').delay(600).fadeIn();
+      $('.anim-s2-2').delay(1200).fadeIn(100).addClass('fadeInLeft');
     },
 
     'heritage': function () {
-      $('.anim-s3-1').fadeIn(100).addClass('fadeInRight');
-      $('.anim-s3-2').delay(800).fadeIn(100).addClass('fadeInDown');
+      $('.anim-s3-bg').fadeIn();
 
-      $('.anim-s3-6').delay(1800).fadeIn(100).addClass('fadeInLeft');
-      $('.anim-s3-7').delay(2800).css({display: 'inline-block', width:0}).animate({width:100}, 700);
-      $('.anim-s3-8').delay(3600).fadeIn(100).addClass('fadeInDown');
+      $('.anim-s3-1').delay(600).fadeIn(100).addClass('fadeInRight');
+      $('.anim-s3-2').delay(1400).fadeIn(100).addClass('fadeInDown');
+
+      $('.anim-s3-6').delay(2400).fadeIn(100).addClass('fadeInLeft');
+      $('.anim-s3-7').delay(3400).css({display: 'inline-block', width:0}).animate({width:100}, 700);
+      $('.anim-s3-8').delay(4200).fadeIn(100).addClass('fadeInDown');
     },
 
     'islands': function () {
-      $('.anim-s4-1').fadeIn();
+      $('.anim-s4-bg').fadeIn();
+
+      $('.anim-s4-1').delay(600).fadeIn();
     },
 
     'coffee': function () {
-      $('.anim-s5-1').fadeIn(100).addClass('fadeInLeft');
-      $('.anim-s5-2').delay(800).css({display: 'inline-block', width:0}).animate({width:100}, 700);
-      $('.anim-s5-3').delay(1800).fadeIn(100).addClass('fadeInDown');
-      $('.anim-s5-4').delay(2800).fadeIn();
+      $('.anim-s5-bg').fadeIn();
+
+      $('.anim-s5-1').delay(600).fadeIn(100).addClass('fadeInLeft');
+      $('.anim-s5-2').delay(1400).css({display: 'inline-block', width:0}).animate({width:100}, 700);
+      $('.anim-s5-3').delay(2400).fadeIn(100).addClass('fadeInDown');
+      $('.anim-s5-4').delay(3200).fadeIn();
     },
 
     'brewing': function () {
+      $('.anim-s6-bg').fadeIn();
     },
 
     'artisan': function () {
-      $('.anim-s7-1').fadeIn();
+      $('.anim-s7-bg').fadeIn();
+
+      $('.anim-s7-1').delay(600).fadeIn();
     },
 
     'subscribe': function () {
-      $('.anim-s8-1').fadeIn();
+      $('.anim-s8-bg').fadeIn();
+
+      $('.anim-s8-1').delay(600).fadeIn();
     },
   };
 
